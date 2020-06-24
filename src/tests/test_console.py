bd3a0833b44c7b926eb09b4bb1ca66c6c64c5b8a
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-06-23 22:00:54 -0500

    Finish 2nd part of hyper-modern tutorial

--- src/tests/test_console.py
+++ src/tests/test_console.py
@@ -42,3 +42,15 @@ def test_main_prints_message_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
+
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hypermodern_python.wikipedia.random_page")
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
+
+@pytest.mark.e2e
+def test_main_succeeds_in_production_env(runner):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
