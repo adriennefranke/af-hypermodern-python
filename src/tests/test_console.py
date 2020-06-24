b65c251896cd309b6e9cc95ac93c2eb9e6b0cdc1
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-06-23 22:03:18 -0500

    Fix errors

--- src/tests/test_console.py
+++ src/tests/test_console.py
@@ -45,7 +45,7 @@ def test_main_prints_message_on_request_error(runner, mock_requests_get):
 
 @pytest.fixture
 def mock_wikipedia_random_page(mocker):
-    return mocker.patch("hypermodern_python.wikipedia.random_page")
+    return mocker.patch("af_hypermodern_python.wikipedia.random_page")
 def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
     runner.invoke(console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
